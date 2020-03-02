56d6963900ba15de676ad83183cc3d50f1bb2cdc
Krutarth Patel <kr739904@dal.ca>
2020-03-02 16:32:44 -0400

    Initial commit

---
+++ demo_service/src/main/java/DBConnectivity/ConnectionParameters.java
@@ -0,0 +1,11 @@
+package DBConnectivity;
+
+public class ConnectionParameters {
+	  
+	 protected static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	 protected static String DEV_SERVER_URL="hubspot.c5o1xp6n01w3.us-east-1.rds.amazonaws.com";
+	 protected static String DEV_SERVER_DATABASE_NAME = "auth_service";
+	 protected static String DEV_SERVER_DATABSE_USER_NAME = "group5";
+	 protected static String DEV_SERVER_DATABSE_PASSWORD = "group5@dal";
+	  
+	}
